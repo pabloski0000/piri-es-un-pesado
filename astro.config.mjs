6a6ad86ed683abef9771028a783e105eba9cd2ed
pabloski <plajommj@gmail.com>
2024-10-03 18:37:01 +0200

    snapshot

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pabloski0000.github.io/portfolio.github.io/',
+  site: 'https://pabloski0000.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
   outDir: 'docs',
   base: "/portfolio.github.io/"
