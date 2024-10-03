ff03c592635d8f7803b7514d305c07c911f2f2d2
pabloski <plajommj@gmail.com>
2024-10-03 13:13:44 +0200

    snapshot

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Pabloski Portfolio Website';
-export const SITE_DESCRIPTION = 'This is my personal portfolio website built with Astro, CSS and TailwindCSS';
+export const SITE_DESCRIPTION = 'My personal portfolio website built with Astro, CSS and TailwindCSS';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
